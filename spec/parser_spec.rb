5edb0a28b50291f7e200fa0b465e500d83dc598c
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-05 18:39:01 +0100

    whish to have tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,7 +1,21 @@
 describe 'Parser' do
-  describe '' do
-    it '' do
-      
+  describe '.parse_log_file' do
+    it 'should be able to add new views' do
+      expect(false).to eq(true)
+    end
+    it 'should be able to add new views' do
+      expect(false).to eq(true)
+    end
+    it 'should be able to add new views' do
+      expect(false).to eq(true)
+    end
+    it 'should be able to add new views' do
+      expect(false).to eq(true)
+    end
+  end
+  describe '.read_views' do
+    it 'print the views in the right format' do
+      expect(false).to eq(true)
     end
   end
 end
