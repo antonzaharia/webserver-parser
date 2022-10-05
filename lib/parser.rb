a4fd51b67b58167ab4e96e0f5b36bafc370ba030
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-05 19:37:47 +0100

    parser printing stuff

--- lib/parser.rb
+++ lib/parser.rb
@@ -13,13 +13,30 @@ class Parser
       page = @views.find { |view| view[:path] == path }
       if page
         if page[:ips].include?(ip)
+          puts "IP #{ip} revisited #{path}"
         else
+          puts "New unique visit for page #{path}"
           page[:ips] << ip
           page[:count] += 1
         end
       else
+        puts "New visit #{path}"
         @views << { path:, ips: [ip], count: 1 }
       end
+    end
+  end
+
+  def order_views
+    @views.sort_by { |view| -view[:count] }
+  end
+
+  def humanize_view(view)
+    "#{view[:path]} #{view[:count]} unique views"
+  end
+
+  def read_views
+    order_views.each do |view|
+      puts humanize_view(view)
     end
   end
 end
