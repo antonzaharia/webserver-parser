81f6a588ce23e137f2b97848cf66911e54706317
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-05 18:53:11 +0100

    tests for Parser class

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,16 +1,35 @@
+require './lib/parser.rb'
+
 describe 'Parser' do
   describe '.parse_log_file' do
+    let(:test1) { 'test1.log' }
+    let(:test2) { 'test2.log' }
+    let(:test3) { 'test3.log' }
+    let(:test4) { 'test4.log' }
+
     it 'should be able to add new view' do
-      expect(false).to eq(true)
+      parser = Parser.new(test1)
+      parser.parse_log_file
+
+      expect(parser.views.size).to eq(1)
     end
     it 'should be able to add views to different pages' do
-      expect(false).to eq(true)
+      parser = Parser.new(test2)
+      parser.parse_log_file
+
+      expect(parser.views.size).to eq(2)
     end
     it 'should be able to increase the count on the same views' do
-      expect(false).to eq(true)
+      parser = Parser.new(test3)
+      parser.parse_log_file
+
+      expect(parser.views.first[:count]).to eq(2)
     end
     it 'should not count duplicated views' do
-      expect(false).to eq(true)
+      parser = Parser.new(test4)
+      parser.parse_log_file
+
+      expect(parser.views.first[:count]).to eq(2)
     end
   end
   describe '.read_views' do
