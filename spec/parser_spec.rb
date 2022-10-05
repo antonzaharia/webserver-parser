b912e6f3a0e7d02d94f5fc479852e061aeb5dd85
Anton Zaharia <zaharia_anton@yahoo.com>
2022-10-05 18:41:26 +0100

    updated tests mesages

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,15 +1,15 @@
 describe 'Parser' do
   describe '.parse_log_file' do
-    it 'should be able to add new views' do
+    it 'should be able to add new view' do
       expect(false).to eq(true)
     end
-    it 'should be able to add new views' do
+    it 'should be able to add views to different pages' do
       expect(false).to eq(true)
     end
-    it 'should be able to add new views' do
+    it 'should be able to increase the count on the same views' do
       expect(false).to eq(true)
     end
-    it 'should be able to add new views' do
+    it 'should not count duplicated views' do
       expect(false).to eq(true)
     end
   end
